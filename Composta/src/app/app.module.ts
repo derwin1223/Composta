dd4171042f33e6fb2bb0627eea0d3e6fb5595320
derwin1223 <derwinmoran12@gmail.com>
2022-11-26 10:07:45 -0400

    Update

---
+++ Composta/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuAllComponent } from './menu-all/menu-all.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    MenuAllComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ] 
+})
+export class AppModule { }
---
+++ Composta/src/app/menu-all/menu-all.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-all',
+  templateUrl: './menu-all.component.html',
+  styleUrls: ['./menu-all.component.scss']
+})
+export class MenuAllComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
